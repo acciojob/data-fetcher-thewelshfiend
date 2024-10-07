966ca4281584c2c1010c8e67d56ffb29ed3b1467
Kiran TJ <kiran.thomas.jacob@gmail.com>
2024-10-07 17:33:44 +0530

    Commit 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,39 @@
-
 import React from "react";
 import './../styles/App.css';
+import { useState, useEffect } from "react";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    const [productData, setProductData] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            const data = await fetch('https://dummyjson.com/products');
+            const json = await data.json();
+
+            setProductData(json);
+        }
+
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            {/* Do not remove the main div */}
+            {productData ? (
+                <pre>
+                    {productData}
+                </pre>
+            ) : (
+                <Loader />
+            )}
+        </div>
+    )
 }
 
 export default App
+
+const Loader = () => {
+    return (
+        <div>Loading...</div>
+    )
+}
